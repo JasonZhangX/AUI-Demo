27adc17b0a1f2f61cfed8e0ef0c0adead7454113
Jason Zhang <v-zhlong@microsoft.com>
2017-02-17 17:38:31 +0800

    Add Wechat Page
    Add Wechat Page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Jason\'s Test Server' });
+});
+
+/* Webchat Research page*/
+app.get('/webchat',function(req,res){
+  res.sendFile(path.join(__dirname+'/html/wechat-small-program.html'));
+  //__dirname : It will resolve to your project folder.
 });
 
 module.exports = router;
