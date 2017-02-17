f47317f086c4040577c4f11b47e579f94b752cbc
Jason Zhang <v-zhlong@microsoft.com>
2017-02-17 17:58:05 +0800

    Add html
    Add html

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var path = require("path");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Jason Test Server' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Jason Test Server'
+  });
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + 'html/wechat-small-program.html'));
 });
 
 module.exports = router;
